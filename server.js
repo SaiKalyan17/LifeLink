704a6e62a4d5e6b20558d3a6d401d0a1391fc1e8
SaiKalyan17 <148944451+SaiKalyan17@users.noreply.github.com>
2025-12-05 11:52:46 +0530

    Fix variable name for server port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors())
 
 app.use("/api/user",userRoutes)
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port: ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port: ${port}`);
 });
 dbConnection();
